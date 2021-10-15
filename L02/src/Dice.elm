d1529592ba17e439c389320bf088a03b1b46ec2b
ShinonL <laura@x-land.ro>
2021-10-15 14:17:00 +0300

    Lab 2

--- L02/src/Dice.elm
+++ L02/src/Dice.elm
@@ -3,7 +3,7 @@ module Dice exposing (..)
 type Die = One | Two | Three | Four | Five | Six
 
 type alias DicePair = { die_1: Die, die_2: Die }
-type DicePair_Def = DicePair_Def Die Die
+type DicePair_Def = Dice Die Die
 
 luckyRoll: DicePair -> String
 luckyRoll dice = 
